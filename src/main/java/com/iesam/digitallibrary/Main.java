70592cc98b4c0493dcfdc0039b28a558e9fa58f7
Juan Camilo Zuñiga Zuñiga <146753356+JCzunzun@users.noreply.github.com>
2024-04-25 16:01:33 +0200

    Feature/1/crear usuario (#6)
    * partially creating the registration of a user
    
    * add remote
    
    * Correct the creation of the file
    
    * Correct the creation of the file

--- src/main/java/com/iesam/digitallibrary/Main.java
+++ src/main/java/com/iesam/digitallibrary/Main.java
@@ -1,19 +1,23 @@
 package com.iesam.digitallibrary;
 
+import com.iesam.digitallibrary.user.presentation.UserPresentation;
+
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner sc= new Scanner(System.in);
+        System.out.println("0: Para salir " +
+                "\n1: Para acceder al apartado de usuarios");
+        int opcion= sc.nextInt();
+        switch (opcion){
+            case 0:
+                break;
+            case 1:
+                UserPresentation.menu();
+                break;
         }
     }
 }
