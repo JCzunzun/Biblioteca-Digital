e6913a93c72074673be28fa679655dbce958e0d4
Juan Camilo Zuñiga Zuñiga <146753356+JCzunzun@users.noreply.github.com>
2024-05-09 16:41:59 +0200

    Test: Create user test (#33)
    verify that the correct method is called the correct times when creating the test

---
+++ src/test/java/com/iesam/digitallibrary/user/domain/CreateUserUseCaseTest.java
@@ -0,0 +1,35 @@
+package com.iesam.digitallibrary.user.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class CreateUserUseCaseTest {
+    @Mock
+    UserRepository userRepository;
+
+    CreateUserUseCase createUserUseCase;
+    @BeforeEach
+    public void setUp(){
+        createUserUseCase= new CreateUserUseCase(userRepository);
+    }
+    @AfterEach
+    public void clear(){
+        createUserUseCase=null;
+    }
+    @Test
+    public void reciboUnUsuarioYLoGuardo(){
+        //Given
+        User user= new User("1","2","juan","amigo@amigo","617929803","calle","nada");
+        //When
+        createUserUseCase.execute(user);
+        //Then
+        Mockito.verify(userRepository, Mockito.times(1)).createUser(user);
+    }
+}
