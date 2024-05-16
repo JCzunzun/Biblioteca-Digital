09f09cf49bb69a887db7a4caed1b79b58f15840f
Juan Camilo Zuñiga Zuñiga <146753356+JCzunzun@users.noreply.github.com>
2024-05-16 12:18:56 +0200

    Test/40/crud loan (#45)
    * Test: loan test
    first 3 test
    
    * Test: loan test
    Create all test for loan functions

---
+++ src/test/java/com/iesam/digitallibrary/loan/domain/EndedLoanUseCaseTest.java
@@ -0,0 +1,39 @@
+package com.iesam.digitallibrary.loan.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class EndedLoanUseCaseTest {
+    @Mock
+    LoanRepository loanRepository;
+
+    EndedLoanUseCase endedLoanUseCase;
+
+    @BeforeEach
+    public void setUp(){
+        endedLoanUseCase=new EndedLoanUseCase(loanRepository);
+    }
+    @AfterEach
+    public void clear(){
+        endedLoanUseCase=null;
+    }
+
+    @Test
+    public void meDanUnIdYModificoElPrestamoDelId(){
+        //Given
+        String idModi="2";
+
+        //When
+        endedLoanUseCase.execute(idModi);
+
+        //Then
+        Mockito.verify(loanRepository,Mockito.times(1)).endedLoan(idModi);
+    }
+}
---
+++ src/test/java/com/iesam/digitallibrary/loan/domain/GetLoansPendingUseCaseTest.java
@@ -0,0 +1,68 @@
+package com.iesam.digitallibrary.loan.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class GetLoansPendingUseCaseTest {
+    @Mock
+    LoanRepository loanRepository;
+
+    GetLoansPendingUseCase getLoansPendingUseCase;
+    @BeforeEach
+    public void setUp(){
+        getLoansPendingUseCase=new GetLoansPendingUseCase(loanRepository);
+    }
+    @AfterEach
+    public void clear(){
+        getLoansPendingUseCase=null;
+    }
+    @Test
+    public void siReciboUnaListaLaCompruebo(){
+        //Given
+        Loan loan1= new Loan("1","1","1");
+        loan1.setStatusLoan("Pending");
+        Loan loan2= new Loan("2","2","2");
+        loan2.setStatusLoan("Pending");
+        ArrayList<Loan> loansExpected= new ArrayList<>();
+        loansExpected.add(loan1);
+        loansExpected.add(loan2);
+        Mockito.when(loanRepository.obtainLoansPending()).thenReturn(loansExpected);
+
+        //When
+        ArrayList<Loan> loansReceived=getLoansPendingUseCase.execute();
+
+        //Then
+        Assertions.assertEquals(loansExpected.size(),loansReceived.size());
+
+        Assertions.assertEquals(loansReceived.get(0).getIdLoan(),"1");
+        Assertions.assertEquals(loansReceived.get(0).getIdUser(),"1");
+        Assertions.assertEquals(loansReceived.get(0).getIdDigitalResource(),"1");
+        Assertions.assertEquals(loansReceived.get(0).getStatusLoan(),"Pending");
+        Assertions.assertEquals(loansReceived.get(1).getIdLoan(),"2");
+        Assertions.assertEquals(loansReceived.get(1).getIdUser(),"2");
+        Assertions.assertEquals(loansReceived.get(1).getIdDigitalResource(),"2");
+        Assertions.assertEquals(loansReceived.get(1).getStatusLoan(),"Pending");
+    }
+
+    @Test
+    public void siNoReciboUnaListaDevuelvoNulo(){
+        //Given
+        Mockito.when(loanRepository.obtainLoansPending()).thenReturn(null);
+
+        //When
+        ArrayList<Loan> loansReceived=loanRepository.obtainLoansPending();
+
+        //Then
+        Assertions.assertNull(loansReceived);
+    }
+}
