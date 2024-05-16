cb6f700688546ba06903d63ddee056563ce2b824
Juan Camilo Zuñiga Zuñiga <146753356+JCzunzun@users.noreply.github.com>
2024-05-16 13:11:44 +0200

    Chore: Changing save mode of book for digital resource (#47)
    connect with DigitalResourceLocalDataSource for use that file

---
+++ src/main/java/com/iesam/digitallibrary/digitalresources/data/local/DigitalResourceFileLocalDataSource.java
@@ -0,0 +1,29 @@
+package com.iesam.digitallibrary.digitalresources.data.local;
+
+import com.google.gson.Gson;
+import com.iesam.digitallibrary.digitalresources.domain.DigitalResource;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DigitalResourceFileLocalDataSource implements DigitalResourceLocalDataSource{
+    private String nameFile="DigitalResource.txt";
+    private Gson gson = new Gson();
+    @Override
+    public void createDigitalResource(DigitalResource digitalResource) {
+        try {
+            File ficheroResource = new File(nameFile);
+            if (!ficheroResource.exists()) {
+                ficheroResource.createNewFile();
+            }
+            FileWriter myWriter = new FileWriter(nameFile,true);
+            myWriter.write(gson.toJson(digitalResource)+System.lineSeparator());
+            myWriter.close();
+            System.out.println("Se logro guardar el usuario");
+        } catch (IOException e) {
+            System.out.println("Ha ocurrido un error al guardar la informacion");
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/iesam/digitallibrary/digitalresources/domain/book/data/BookDataRepository.java
+++ src/main/java/com/iesam/digitallibrary/digitalresources/domain/book/data/BookDataRepository.java
@@ -1,5 +1,6 @@
 package com.iesam.digitallibrary.digitalresources.domain.book.data;
 
+import com.iesam.digitallibrary.digitalresources.data.local.DigitalResourceLocalDataSource;
 import com.iesam.digitallibrary.digitalresources.domain.book.data.local.BookLocalDataSource;
 import com.iesam.digitallibrary.digitalresources.domain.book.domain.Book;
 import com.iesam.digitallibrary.digitalresources.domain.book.domain.BookRepository;
@@ -7,27 +8,27 @@ import com.iesam.digitallibrary.digitalresources.domain.book.domain.BookReposito
 import java.util.ArrayList;
 
 public class BookDataRepository implements BookRepository {
-    BookLocalDataSource bookLocalDataSource;
-    public BookDataRepository(BookLocalDataSource bookLocalDataSource){
-        this.bookLocalDataSource= bookLocalDataSource;
+    DigitalResourceLocalDataSource digitalResourceLocalDataSource;
+    public BookDataRepository(DigitalResourceLocalDataSource digitalResourceLocalDataSource){
+        this.digitalResourceLocalDataSource= digitalResourceLocalDataSource;
     }
     @Override
     public void createBook(Book book) {
-        bookLocalDataSource.createBook(book);
+        digitalResourceLocalDataSource.createDigitalResource(book);
     }
 
     @Override
     public void deleteBook(String id) {
-        bookLocalDataSource.deleteBook(id);
+
     }
 
     @Override
     public void modifiedBook(Book book) {
-        bookLocalDataSource.modifiedBook(book);
+
     }
 
     @Override
     public ArrayList<Book> getsBooks() {
-        return bookLocalDataSource.getsBooks();
+        return null;
     }
 }
